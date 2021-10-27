45c07a76b8260e37e109ba093eefef70bab947f0
Taleb27 <a7la.taleb.atta@gmail.com>
2021-10-27 14:27:13 +0300

    ADDED TO STRING

--- app/src/main/java/y2020/aoc/bayan/taleb/MainActivity.java
+++ app/src/main/java/y2020/aoc/bayan/taleb/MainActivity.java
@@ -1,24 +1,35 @@
 package y2020.aoc.bayan.taleb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnLongClickListener  {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity<email, password> extends AppCompatActivity implements View.OnLongClickListener  {
+
+    private static final String TAG = "FIREBASE";
     private Button buttonlogin;
     private EditText editTextName, editTetPassword;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Initialize Firebase Auth(return refrence to the instance of the project firebase
+        mAuth = FirebaseAuth.getInstance();
         editTextName = findViewById(R.id.editTextName);
         editTetPassword = findViewById(R.id.editTextPassword);
         buttonlogin = findViewById(R.id.buttonlogin);
@@ -36,8 +47,9 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
     }
 
+
     public void login(View view) {
-        Intent intent = new Intent(this, WelcomeActivity.class);
+
         if (!editTextName.getText().toString().equals("")) {
             //create sp file
             SharedPreferences Settings = getSharedPreferences("settings",MODE_PRIVATE);
@@ -49,9 +61,12 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
            //save and close file
             editor.commit();
 
+           // intent.putExtra("name",editTextName.getText().toString());
 
-            intent.putExtra("name",editTextName.getText().toString());
-            startActivity(intent);
+            login(editTextName.getText().toString(),editTetPassword.getText().toString());
+
+
+
         }
     }
 
@@ -62,4 +77,31 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         editTetPassword.setText("");
         return true;
     }
+    public void login(String email,String Password){}
+      mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete boolean task;
+        (@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                Intent i  = new Intent(MainActivity.this,WelcomeActivity.this);
+                startActivity(i);
+
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                Toast.makeText(MainActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            //
+        }
+
+
+    });
+}
+
 }
--- app/src/main/java/y2020/aoc/bayan/taleb/Sign_Activity.java
+++ app/src/main/java/y2020/aoc/bayan/taleb/Sign_Activity.java
@@ -1,27 +1,102 @@
 package y2020.aoc.bayan.taleb;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.telephony.PhoneNumberUtils;
-
-import java.net.PasswordAuthentication;
-
-public class Sign_Activity extends AppCompatActivity {
-    private String Username;
-    private int PersonNum;
-    private String email;
-    private String Pass;
-    private String RePass;
-    private String City;
-    private int age;
-
-
+public class Sign_Activity extends AppCompatActivity implements View.OnLongClickListener {
+private FirebaseAuth mAuth;
+    EditText firstName;
+    EditText lastName;
+    EditText address;
+    EditText email;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_);
+        setContentView(R.layout.activity_main);
+     mAuth = FirebaseAuth.getInstance()
+   /*     firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        address = findViewById(R.id.address);
+        email = findViewById(R.id.email);
+        register = findViewById(R.id.register);
+*/
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
+            }
+        });
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered() {
+        if (isEmpty(firstName)) {
+            Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isEmpty(lastName)) {
+            lastName.setError("Last name is required!");
+        }
+
+        if (isEmail(email) == false) {
+            email.setError("Enter valid email!");
+        }
+
+    }
+    public void submit(View view){
+        signup(editTextName.getText().toString(),editTextPassword.getText().toString());
+    }
+    public void signup(String email,String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i  = new Intent(MainActivity.this,WelcomeActivity.this);
+                            startActivity(i);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        //
+                    }
+
+
+                });
     }
 }
